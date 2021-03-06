58591f097b21124a9a052dfabba50faf74d04a6b
Tingchenlee <lee.ting@northeastern.edu>
2021-03-05 21:15:12 -0500

    correct unit of A factor in Ammonia_Pt111_oxidation

--- input/kinetics/libraries/Surface/Ammonia_Pt111_oxidation/reactions.py
+++ input/kinetics/libraries/Surface/Ammonia_Pt111_oxidation/reactions.py
@@ -4,128 +4,314 @@
 name = "Ammonia_Pt111_oxidation"
 shortDesc = u""
 longDesc = u"""
-Based primarily on DMa, Hanyu; Schneider, William F. (2019)
+This library is built to import training reactions, based on:
 "Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
-ACS Publications. Journal contribution. 
-https://doi.org/10.1021/acscatal.8b04251.s001 
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
 """
 
-#Author Imaginary prefactors of the reactions at 300K
 entry(
     index = 1,
-    label = " NH3 + X <=> NH3_X ",
+    label = "O2 + X + X <=> O_X + O_X",
     kinetics = StickingCoefficient(
-        A=2.5E-2,                
-        n = 0.0,
-        Ea = (0, 'kJ/mol'),  
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        A = 1.8E-2,
+        n = 0,
+        Ea = (0, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""O2 Surface_Adsorption_Dissociative""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+The pressure of adsorption processes: P= 1 bar = 10E5 Pa
 """,
-    metal = "Pt" , 
+	metal = "Pt",
+    facet = "111",
 )
 
 entry(
     index = 2,
-    label = " NH3_X +O_X <=> NH2_X + OH_X ",
-    kinetics = SurfaceArrhenius(
-        A=(8.4E12, 'm^2/(mol*s)'), #8.4x10^12 (1/s)
-        n = 0.0,
-        Ea = (67.51, 'kJ/mol'),    #0.7eV
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+    label = "NH3 + X <=> NH3_X",
+    kinetics = StickingCoefficient(
+        A = 2.5E-2,
+        n = 0,
+        Ea = (0, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""Surface_Adsorption_vdW""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+The pressure of adsorption processes: P= 1 bar = 10E5 Pa
 """,
-    metal = "Pt" ,
+    metal = "Pt",
+    facet = "111",
 )
 
 entry(
     index = 3,
-    label = " NH2_X +O_X <=> NH_X + OH_X ",
+    label = "NH3_X +O_X <=> NH2_X + OH_X",
     kinetics = SurfaceArrhenius(
-        A=(4.2E12, 'm^2/(mol*s)'), #4.2x10^12 (1/s)
+        A = (5.2E12, 'cm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (78.12, 'kJ/mol'),   #0.81eV
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (67543, 'J/mol'),   
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""Surface_Abstraction_vdW""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
 """,
-    metal = "Pt" ,
+    metal = "Pt",
+    facet = "111",
 )
 
 entry(
     index = 4,
-    label = " NH_X +O_X <=> N_X + OH_X ",
+    label = "NH2_X +O_X <=> NH_X + OH_X",
     kinetics = SurfaceArrhenius(
-        A=(3.8E12, 'm^2/(mol*s)'), #3.8x10^12 (1/s)
+        A = (5.5E12, 'cm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (154.30, 'kJ/mol'),   #1.6eV
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (78157, 'J/mol'),   
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""Surface_Abstraction""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
 """,
-    metal = "Pt" ,
+    metal = "Pt",
+    facet = "111",
 )
 
 entry(
     index = 5,
-    label = " NH3_X + OH_X <=> NH2_X + H2O_X  ",
+    label = "NH_X + O_X <=> N_X + OH_X",
     kinetics = SurfaceArrhenius(
-        A=(3.2E12, 'm^2/(mol*s)'), #3.2x10^12 (1/s)
+        A = (7.8E12, 'cm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (33.75, 'kJ/mol'),   #0.35eV
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (154380, 'J/mol'),   
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""Surface_Abstraction""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
 """,
-    metal = "Pt" ,
+    metal = "Pt",
+    facet = "111",
 )
 
 entry(
     index = 6,
-    label = " NH2_X + OH_X <=> NH_X + H2O_X  ",
+    label = "NH3_X + OH_X <=> NH2_X + H2O_X",
     kinetics = SurfaceArrhenius(
-        A=(4.7E12, 'm^2/(mol*s)'), #4.7x10^12 (1/s)
+        A = (4.9E13, 'cm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (0.96, 'kJ/mol'),  #0.01
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (33772, 'J/mol'),   
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""Surface_Abstraction_Single_vdW""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
 """,
-    metal = "Pt" ,
+    metal = "Pt",
+    facet = "111",
 )
 
 entry(
     index = 7,
-    label = " NH_X + OH_X <=> N_X + H2O_X  ",
+    label = "NH2_X + OH_X <=> NH_X + H2O_X",
     kinetics = SurfaceArrhenius(
-        A=(1.2E12, 'm^2/(mol*s)'), #1.2x10^12 (1/s)
+        A = (1.5E13, 'cm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (39.54, 'kJ/mol'),  #0.41eV 
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (965, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""Default""",
+    shortDesc = u"""Surface_Abstraction_vdW""",
     longDesc = u"""
-Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities.
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
 """,
-    metal = "Pt" ,
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 8,
+    label = "NH_X + OH_X <=> N_X + H2O_X",
+    kinetics = SurfaceArrhenius(
+        A = (5.9E12, 'cm^2/(mol*s)'), 
+        n = 0.0,
+        Ea = (39561, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Surface_Abstraction_vdW""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 9,
+    label = "OH_X + OH_X <=> O_X + H2O_X",
+    kinetics = SurfaceArrhenius(
+        A = (3.1E12, 'cm^2/(mol*s)'),  
+        n = 0.0,
+        Ea = (0, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Reverse R95""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 10,
+    label = "H2O_X <=> H2O + X",
+    kinetics = SurfaceArrhenius(
+        A = (5.9E15, 'cm^2/(mol*s)'),  
+        n = 0.0,
+        Ea = (18333, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Surface_Adsorption""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+A = 5.9E15(1/s) = 2.376E24(cm^2/(mol*s))
+""",
+	metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 11,
+    label = "N_X + N_X <=> N2 + X + X",
+    kinetics = SurfaceArrhenius(
+        A = (9.2E12, 'cm^2/(mol*s)'),  
+        n = 0.0,
+        Ea = (244119, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""N2 Surface_Adsorption_Dissociative""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 12,
+    label = "N_X + O_X <=> NO_X + X",
+    kinetics = SurfaceArrhenius(
+        A = (8.3E12, 'cm^2/(mol*s)'),  
+        n = 0.0,
+        Ea = (213243, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Nitrogen/51""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 13,
+    label = "NO2 + X <=> NO2_X",
+    kinetics = StickingCoefficient(
+        A = 1.9E-2,  
+        n = 0.0,
+        Ea = (184296, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Surface_Adsorption_Single""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+The pressure of adsorption processes: P= 1 bar = 10E5 Pa
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 14,
+    label = "N_X + NO_X <=> N2O_X + X",
+    kinetics = SurfaceArrhenius(
+        A = (4.3E12, 'cm^2/(mol*s)'),   
+        n = 0.0,
+        Ea = (164998, 'J/mol'), 
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Reverse R34""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+""",
+    metal = "Pt",
+    facet = "111",
+)
+
+entry(
+    index = 15,
+    label = "N2O + X <=> N2O_X",
+    kinetics = StickingCoefficient(
+        A = 1.6E-2,  
+        n = 0.0,
+        Ea = (0, 'J/mol'),  
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
+    ),
+    shortDesc = u"""Surface_Adsorption_Double/Surface_Adsorption_vdW""",
+    longDesc = u"""
+"Structure- and Temperature-Dependence of Pt-Catalyzed Ammonia Oxidation Rates and Selectivities."
+DMa, Hanyu; Schneider, William F.(2019). ACS Catalysis, 9(3), 2407-2414. 
+https://doi.org/10.1021/acscatal.8b04251
+The pressure of adsorption processes: P= 1 bar = 10E5 Pa
+""",
+    metal = "Pt",
+    facet = "111",
 )
--- input/kinetics/libraries/Surface/Ammonia_Pt3_eletro_oxidation/reactions.py
+++ input/kinetics/libraries/Surface/Ammonia_Pt3_eletro_oxidation/reactions.py
@@ -5,129 +5,167 @@ name = "Ammonia_Pt3_eletro_oxidation"
 shortDesc = u""
 longDesc = u"""
 Based primarily on:
-"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium_ 
-A theoretical approach _ Elsevier Enhanced Reade"
+"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium: A theoretical approach"
+Ali Estejab, Gerardine G.Botte(2018). Molecular Catalysis, 445, 279-292.
 https://doi.org/10.1016/j.mcat.2017.11.025
+
+The A factor is calculated by Eqs.10 with the provided Ea and rate constants
+metal = Pt3 cluster
 """
 #The values of the A factor and Ea are calculated in the excel file
 #The reaction mechanisms of ammonia oxidation are not similar to other papers
 entry(
     index = 1,
-    label = " NH3_X + X <=> NH2_X + H_X ",
+    label = "NH3_X + X <=> NH2_X + H_X",
     kinetics = SurfaceArrhenius(
         A = (1.21E12, 'm^2/(mol*s)'), 
         n = 0.0,
         Ea = (142.81, 'kJ/mol'),
-        Tmin = (298, 'K'),
+        Tmin = (3000, 'K'),
         Tmax = (2000, 'K'),
     ),
     shortDesc = u"""""",
     longDesc = u"""""",
-    metal = "Pt" ,
+    metal = "Pt",
 )
 
 entry(
     index = 2,
-    label = " NH2_X + NH2_X <=> N2H4_X + X ",
+    label = "NH2_X + NH2_X <=> N2H4_X + X",
     kinetics = SurfaceArrhenius(
         A = (1.21E23, 'm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (206.49, 'kJ/mol'),
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (206490, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""""",
-    longDesc = u"""""",
-    metal = "Pt" ,
+    shortDesc = u"""Surface_Dissociation_vdW""",
+    longDesc = u"""
+"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium: A theoretical approach"
+Ali Estejab, Gerardine G.Botte(2018). Molecular Catalysis, 445, 279-292.
+https://doi.org/10.1016/j.mcat.2017.11.025
+
+The A factor is calculated by Eqs.10 with the provided Ea and rate constants
+metal = Pt3 cluster
+""",
+    metal = "Pt",
 )
 
 entry(
     index = 3,
-    label = " N2H4_X + X <=> N2H3_X + H_X ",
+    label = "N2H4_X + X <=> N2H3_X + H_X",
     kinetics = SurfaceArrhenius(
         A = (2.5E7, 'm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (115.79, 'kJ/mol'),
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (115790, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""""",
-    longDesc = u"""""",
-    metal = "Pt" ,
+    shortDesc = u"""Surface_Dissociation_vdW""",
+    longDesc = u"""
+"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium: A theoretical approach"
+Ali Estejab, Gerardine G.Botte(2018). Molecular Catalysis, 445, 279-292.
+https://doi.org/10.1016/j.mcat.2017.11.025
+
+The A factor is calculated by Eqs.10 with the provided Ea and rate constants
+metal = Pt3 cluster
+""",
+    metal = "Pt",
 )
 
 entry(
     index = 4,
-    label = " N2H3_X + X <=> N2H2_X + H_X ",
+    label = "N2H3_X + X <=> N2H2_X + H_X",
     kinetics = SurfaceArrhenius(
         A = (1.31E5, 'm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (103.24, 'kJ/mol'),
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (103240, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""""",
-    longDesc = u"""""",
-    metal = "Pt" ,
+    shortDesc = u"""Surface_Dissociation""",
+    longDesc = u"""
+"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium: A theoretical approach"
+Ali Estejab, Gerardine G.Botte(2018). Molecular Catalysis, 445, 279-292.
+https://doi.org/10.1016/j.mcat.2017.11.025
+
+The A factor is calculated by Eqs.10 with the provided Ea and rate constants
+metal = Pt3 cluster
+""",
+    metal = "Pt",
 )
 
 entry(
     index = 5,
-    label = " N2H2_X + X <=> N2H_X + H_X ",
+    label = "N2H2_X + X <=> N2H_X + H_X",
     kinetics = SurfaceArrhenius(
         A = (3.76E11, 'm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (139.91, 'kJ/mol'),
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (139910, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""""",
-    longDesc = u"""""",
-    metal = "Pt" ,
+    shortDesc = u"""Surface_Dissociation_Beta""",
+    longDesc = u"""
+"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium: A theoretical approach"
+Ali Estejab, Gerardine G.Botte(2018). Molecular Catalysis, 445, 279-292.
+https://doi.org/10.1016/j.mcat.2017.11.025
+
+The A factor is calculated by Eqs.10 with the provided Ea and rate constants
+metal = Pt3 cluster
+""",
+    metal = "Pt",
 )
 
-#The A factor and Ea of this mechanism is really difference
 entry(
     index = 6,
-    label = " N2H_X + X <=> N2_X + H_X ",
+    label = "N2H_X + X <=> N2_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (2.49E-13, 'm^2/(mol*s)'), 
+        A = (2.49E13, 'm^2/(mol*s)'), 
         n = 0.0,
-        Ea = (0.96, 'kJ/mol'),
-        Tmin = (298, 'K'),
-        Tmax = (2000, 'K'),
+        Ea = (960, 'J/mol'),
+        Tmin = (200, 'K'),
+        Tmax = (3000, 'K'),
     ),
-    shortDesc = u"""""",
-    longDesc = u"""""",
-    metal = "Pt" ,
+    shortDesc = u"""Surface_Addition_Single_vdW""",
+    longDesc = u"""
+"Ammonia oxidation kinetics on bimetallic clusters of platinum and iridium: A theoretical approach"
+Ali Estejab, Gerardine G.Botte(2018). Molecular Catalysis, 445, 279-292.
+https://doi.org/10.1016/j.mcat.2017.11.025
+
+The A factor is calculated by Eqs.10 with the provided Ea and rate constants
+metal = Pt3 cluster
+""",
+    metal = "Pt",
 )
+
 
 entry(
     index = 7,
-    label = " NH2_X + X <=> NH_X + H_X ",
+    label = "NH2_X + X <=> NH_X + H_X",
     kinetics = SurfaceArrhenius(
         A = (4.81E23, 'm^2/(mol*s)'), 
         n = 0.0,
         Ea = (209.38, 'kJ/mol'),
-        Tmin = (298, 'K'),
+        Tmin = (3000, 'K'),
         Tmax = (2000, 'K'),
     ),
     shortDesc = u"""""",
     longDesc = u"""""",
-    metal = "Pt" ,
+    metal = "Pt",
 )
 
 entry(
     index = 8,
-    label = " NH_X + X <=> N_X + H_X ",
+    label = "NH_X + X <=> N_X + H_X",
     kinetics = SurfaceArrhenius(
         A = (4.57E16, 'm^2/(mol*s)'), 
         n = 0.0,
         Ea = (168.86, 'kJ/mol'),
-        Tmin = (298, 'K'),
+        Tmin = (3000, 'K'),
         Tmax = (2000, 'K'),
     ),
     shortDesc = u"""""",
     longDesc = u"""""",
-    metal = "Pt" ,
+    metal = "Pt",
 )
