d79a8283660683220e0c5097ef46066429178041
Tingchenlee <lee.ting@northeastern.edu>
2021-05-14 19:55:57 -0400

    revised Novell_Pt111 and added Novell_Pd111, Rh111 with modified Arrhenius parameters

--- input/kinetics/libraries/Surface/Novell_Rh111/reactions.py
+++ input/kinetics/libraries/Surface/Novell_Rh111/reactions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-name = "Novell_Pt111"
+name = "Novell_Rh111"
 shortDesc = u""
 longDesc = u"""
 This library is built to import training reactions, based on:
@@ -14,9 +14,9 @@ entry(
     index = 16,
     label = "NH3_X + X <=> NH2_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (1.02E18, 'cm^2/(mol*s)'), 
-        n = 0.911,
-        Ea = (91000, 'J/mol'),
+        A = (1.31E23, 'cm^2/(mol*s)'), 
+        n = -0.791,
+        Ea = (100000, 'J/mol'),
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
     ),
@@ -25,10 +25,10 @@ entry(
 "Ammonia Dehydrogenation over Platinum-Group Metal Surfaces. Structure, Stability, and Reactivity of Adsorbed NHx Species"
 Gerard Novell-Leruth et al. J. Phys. Chem. C 2007, 111, 2, 860–868
 https://doi.org/10.1021/jp064742b
-This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+This reaction used RMG's surface site density of Rh111 = 2.656E-9(mol/cm^2) to calculate the A factor.
 The modified Arrhenius parameter is calculated from TABLE 4.
 """,
-    metal = "Pt",
+    metal = "Rh",
     facet = "111",
 )
 
@@ -36,9 +36,9 @@ entry(
     index = 17,
     label = "NH2_X + X <=> NH_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (2.48E18, 'cm^2/(mol*s)'), 
-        n = 1.202,
-        Ea = (101000, 'J/mol'),
+        A = (1.23E19, 'cm^2/(mol*s)'), 
+        n = 0.902,
+        Ea = (84000, 'J/mol'),
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
     ),
@@ -47,10 +47,10 @@ entry(
 "Ammonia Dehydrogenation over Platinum-Group Metal Surfaces. Structure, Stability, and Reactivity of Adsorbed NHx Species"
 Gerard Novell-Leruth et al. J. Phys. Chem. C 2007, 111, 2, 860–868
 https://doi.org/10.1021/jp064742b
-This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+This reaction used RMG's surface site density of Rh111 = 2.656E-9(mol/cm^2) to calculate the A factor.
 The modified Arrhenius parameter is calculated from TABLE 4.
 """,
-    metal = "Pt",
+    metal = "Rh",
     facet = "111",
 )
 
@@ -58,9 +58,9 @@ entry(
     index = 18,
     label = "NH_X + X <=> N_X + H_X",
     kinetics = SurfaceArrhenius(
-        A = (4.21E18, 'cm^2/(mol*s)'), 
-        n = 1.146,
-        Ea = (116000, 'J/mol'),
+        A = (1.1E19, 'cm^2/(mol*s)'), 
+        n = 0.965,
+        Ea = (98000, 'J/mol'),
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
     ),
@@ -69,9 +69,9 @@ entry(
 "Ammonia Dehydrogenation over Platinum-Group Metal Surfaces. Structure, Stability, and Reactivity of Adsorbed NHx Species"
 Gerard Novell-Leruth et al. J. Phys. Chem. C 2007, 111, 2, 860–868
 https://doi.org/10.1021/jp064742b
-This reaction used RMG's surface site density of Pt111 = 2.483E-9(mol/cm^2) to calculate the A factor.
+This reaction used RMG's surface site density of Rh111 = 2.656E-9(mol/cm^2) to calculate the A factor.
 The modified Arrhenius parameter is calculated from TABLE 4.
 """,
-    metal = "Pt",
+    metal = "Rh",
     facet = "111",
 )
